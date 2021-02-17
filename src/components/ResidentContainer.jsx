402013e0442c8afff7d2b841c80c3724a1b3664b
Martin Sg <martindev.mx@outlook.com>
2021-02-17 01:19:02 -0600

    somes unused code deleted

--- src/components/ResidentContainer.jsx
+++ src/components/ResidentContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import ResidentInfo from './ResidentInfo';
 
 const ResidentContainer = ( { residents } ) => {
--- src/helpers/getAllLocations.jsx
+++ src/helpers/getAllLocations.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import getPageLocation from './getPageLocation';
 
